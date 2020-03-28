26784a323b23ab2bcace1284fb038cdf1b9f41e4
Ckongfu <zouguannan0@163.com>
2020-03-28 14:12:32 +0800

    Mooc  第一周

--- python3.7/MOOC/第一周/温度转换.py
+++ python3.7/MOOC/第一周/温度转换.py
@@ -5,7 +5,7 @@ if TempStr[-1] in ['F','f'] : #索引[]，取倒数第一个字符
     C=(eval(TempStr[0:-1])-32)/1.8#eval-评估函数，取消字符串两侧的引号，返回一个数值
     print ('转换以后的温度是{:.2f}C'.format(C))#格式化---将变量C的值取2位小数，然后填充到大括号里面
 
-elif TempStr[-1]:
+elif TempStr[-1]in ['C','c']:
     F=1.8*eval(TempStr[0:-1])+32
     print ('转换后的温度是{:.2f}F'.format(F))
 
