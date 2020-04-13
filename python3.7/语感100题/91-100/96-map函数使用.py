852adf9178383c9244724edb121cc0b0bf2d2af5
Ckongfu <zouguannan0@163.com>
2020-04-13 08:53:29 +0800

    使用循环修改

--- python3.7/语感100题/91-100/96-map函数使用.py
+++ python3.7/语感100题/91-100/96-map函数使用.py
@@ -2,9 +2,17 @@
 def square(x):
     return pow(x,2)
 a=[2,3,4,5]
+b=[]
+for i in a:
+    x=square(i)
+    b.append(x)
+print(b)
+
+'''
+a=[2,3,4,5]
 j=map(square,a)
 print(list(j))
 
 k=map(lambda x:pow(x,2),[2,3,4,5])
 print(list(k))
-
+'''
